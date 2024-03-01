0fdac437f3931d7a5c4c687874de294f31f61ea0
Bartosz Grzybowski <bartoszgrzybo2001@gmail.com>
2024-03-01 08:29:47 +0100

    Dodaj pliki projektów.

---
+++ Platformy programistyczne .NET i Java/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Class1
+{
+    private int upperLimit;
+
+    public Class1(int upperLimit)
+    {
+        this.upperLimit = upperLimit;
+    }
+
+    public void PrintFizzBuzz()
+    {
+        for (int i = 1; i <= upperLimit; i++)
+        {
+            if (i % 5 == 0 && i % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Platformy programistyczne .NET i Java/FizzBuzz.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class FizzBuzz
+{
+    private int upperLimit;
+
+    public FizzBuzz(int upperLimit)
+    {
+        this.upperLimit = upperLimit;
+    }
+
+    public void PrintFizzBuzz()
+    {
+        for (int i = 0; i <= upperLimit; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (i % 5 == 0 && i % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wpisz gorny limit: ");
+        int upperLimit = int.Parse(Console.ReadLine());
+
+        // FizzBuzz fizzBuzz = new FizzBuzz(upperLimit);
+        // fizzBuzz.PrintFizzBuzz();
+
+        Class1 fizzBuzz_2 = new Class1(upperLimit);
+        fizzBuzz_2.PrintFizzBuzz();
+
+    }
+}
